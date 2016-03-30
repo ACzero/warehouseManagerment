9a583df5e5e2acf17466dd1196afb7946ddc7bd1
ACzero <lzh.scut@hotmail.com>
2016-03-30 22:27:22 +0800

    modified bill action

--- app/admin/bill.rb
+++ app/admin/bill.rb
@@ -1,6 +1,19 @@
 ActiveAdmin.register Bill do
   permit_params :total_amount, :total_price, :good_id, :trader_id, :trader_type
 
+  # remove new button on index page
+  config.remove_action_item(:new)
+  # remove new & edit button on other place
+  actions :all, :except => [:new, :edit]
+
+  action_item :add_import_bill, only: [:index] do
+    link_to '添加进货单', new_admin_bill_path(trader_type: "Supplier")
+  end
+
+  action_item :add_sell_bill, only: [:index] do
+    link_to '添加销售单', new_admin_bill_path(trader_type: "Buyer")
+  end
+
   form do |f|
     inputs do
       f.input :trader_type, :as => :hidden, :input_html => { value: assigns[:trader_type] }
