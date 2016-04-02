d7315939cb35dfee4845e5e4243f28002b23cef2
ACzero <lzh.scut@hotmail.com>
2016-04-02 13:42:43 +0800

    add good relationships & statiscis

--- app/admin/good.rb
+++ app/admin/good.rb
@@ -1,18 +1,37 @@
 ActiveAdmin.register Good do
+  permit_params :name, :good_type, :origin, :store, :percentage, :product_id
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  index do
+    column :name
+    column :good_type
+    column :origin
+    column :store
+    column :total do |good|
+      (assigns[:goods_statistics][good.id] || 0) + good.store
+    end
+    column :product
+    column :percentage
+    actions
+  end
 
-permit_params :name, :type, :origin, :store, :percentage, :product_id
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :good_type, :include_blank => false,
+        :collection => [['成品', 'product'], ['原材料', 'raw']]
+      f.input :origin
+      f.input :store
+      f.input :product
+      f.input :percentage
+    end
+    f.actions
+  end
 
+  controller do
+    def index
+      super do |format|
+        @goods_statistics = Good.good_amount_statistics(@goods.pluck(:id))
+      end
+    end
+  end
 end
--- app/models/good.rb
+++ app/models/good.rb
@@ -3,4 +3,44 @@ class Good < ActiveRecord::Base
   belongs_to :product, class_name: "Good"
 
   enum good_type: [ :raw, :product ]
+
+  before_create :check_good_type
+
+  def self.good_amount_statistics(target_goods = nil)
+    statistics = target_goods.nil? ? bills_amount_statistics : bills_amount_statistics.where(good_id: target_goods)
+
+    statistics_arr = statistics.map do |good_sum|
+      [good_sum.good_id, good_sum.total]
+    end
+
+    Hash[statistics_arr]
+  end
+
+  private
+  def self.bills_amount_statistics
+    Bill.joins('LEFT OUTER JOIN deliver_histories on bills.id = deliver_histories.bill_id')
+      .group("bills.good_id").select("bills.good_id,
+        SUM(CASE
+              WHEN bills.trader_type = 'Supplier'
+              THEN deliver_histories.amount
+              ELSE -deliver_histories.amount
+            END) AS total")
+  end
+
+  def check_good_type
+    if self.raw?
+      if self.product.nil?
+        errors.add(:product, '原料商品必须选择成品')
+        return false
+      end
+    elsif self.product?
+      if self.product.present?
+        errors.add(:product, '成品不能设置该项')
+        return false
+      end
+    else
+      errors.add(:good_type, '请设置商品类型')
+      return false
+    end
+  end
 end
