0fb0118ef02fd3c3adf899625cb34067f2327835
ACzero <lzh.scut@hotmail.com>
2016-05-30 21:01:13 +0800

    add stastic

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,26 +2,75 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+  content title: "商品销量统计" do
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    panel "销售总量统计" do
+      table_for BuyerBill.sell_amount_statiscian do
+        column "商品" do |bill|
+          bill[:good].name
+        end
+
+        (7.day.ago.to_date..Time.now.to_date).reverse_each do |date|
+          column date.to_s do |bill|
+            statiscian = bill[:statiscian][date.to_s]
+            statiscian ? statiscian.first.sum_amount : 0.0
+          end
+        end
+      end
+    end
+
+    panel "销售总额统计" do
+      table_for BuyerBill.sell_price_statiscian do
+        column "商品" do |bill|
+          bill[:good].name
+        end
+
+        (7.day.ago.to_date..Time.now.to_date).reverse_each do |date|
+          column date.to_s do |bill|
+            statiscian = bill[:statiscian][date.to_s]
+            statiscian ? statiscian.first.sum_price : 0.0
+          end
+        end
+      end
+    end
+
+    panel "销售总量月统计" do
+      table_for BuyerBill.month_amount_statiscian do
+        column "商品" do |bill|
+          bill[:good].name
+        end
+
+        (1..12).each do |month|
+          column "#{month}月" do |bill|
+            statiscian = bill[:statiscian][month]
+            statiscian ? statiscian.first.sum_amount : 0.0
+          end
+        end
+      end
+    end
+
+    panel "销售总额月统计" do
+      table_for BuyerBill.month_price_statiscian do
+        column "商品" do |bill|
+          bill[:good].name
+        end
+
+        (1..12).each do |month|
+          column "#{month}月" do |bill|
+            statiscian = bill[:statiscian][month]
+            statiscian ? statiscian.first.sum_price : 0.0
+          end
+        end
+      end
+    end
 
     #   column do
     #     panel "Info" do
@@ -29,5 +78,6 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
-  end # content
+
+  end
 end
--- app/models/buyer_bill.rb
+++ app/models/buyer_bill.rb
@@ -1,4 +1,72 @@
 class BuyerBill < Bill
+  def self.sell_amount_statiscian
+    result = BuyerBill.includes(:good).group("good_id, DATE(bills.created_at)")
+     .joins("LEFT OUTER join deliver_histories on deliver_histories.bill_id = bills.id")
+     .select("
+       bills.good_id,
+       DATE(bills.created_at) AS date,
+       SUM(deliver_histories.amount) AS sum_amount
+     ").group_by(&:good)
+
+    result.map do |k, v|
+      {
+        good: k,
+        statiscian: v.group_by { |sum| sum.date.to_s }
+      }
+    end
+  end
+
+  def self.sell_price_statiscian
+    result = BuyerBill.includes(:good).group("good_id, DATE(bills.created_at)")
+     .joins("LEFT OUTER join pay_histories on pay_histories.bill_id = bills.id")
+     .select("
+       bills.good_id,
+       DATE(bills.created_at) AS date,
+       SUM(pay_histories.pay) AS sum_price
+     ").group_by(&:good)
+
+    result.map do |k, v|
+      {
+        good: k,
+        statiscian: v.group_by { |sum| sum.date.to_s }
+      }
+    end
+  end
+
+  def self.month_amount_statiscian
+    result = BuyerBill.includes(:good).group("good_id, EXTRACT(MONTH FROM bills.created_at)")
+     .joins("LEFT OUTER join deliver_histories on deliver_histories.bill_id = bills.id")
+     .select("
+       bills.good_id,
+       EXTRACT(MONTH FROM bills.created_at) AS month,
+       SUM(deliver_histories.amount) AS sum_amount
+     ").group_by(&:good)
+
+    result.map do |k, v|
+      {
+        good: k,
+        statiscian: v.group_by { |sum| sum.month.to_i }
+      }
+    end
+  end
+
+  def self.month_price_statiscian
+    result = BuyerBill.includes(:good).group("good_id, EXTRACT(MONTH FROM bills.created_at)")
+     .joins("LEFT OUTER join pay_histories on pay_histories.bill_id = bills.id")
+     .select("
+       bills.good_id,
+       EXTRACT(MONTH FROM bills.created_at) AS month,
+       SUM(pay_histories.pay) AS sum_price
+     ").group_by(&:good)
+
+    result.map do |k, v|
+      {
+        good: k,
+        statiscian: v.group_by { |sum| sum.month.to_i }
+      }
+    end
+  end
+
   def create_buy_and_deliver
     self.pay_histories.create(pay: self.total_price)
     self.deliver_histories.create(amount: self.total_amount)
